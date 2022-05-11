239c3754ab37b8d057e9178689409093d56115e6
Matt Oliver <matt.oliver@appfire.com>
2022-05-11 14:58:01 -0500

    adds vercel.json with rewrite rule

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,18 @@ export default function Home() {
   return (
     <div>
       <h1>This is the parent site</h1>
+      <p>
+        Its URL is:{" "}
+        <code>https://gatsby-vercel-site-path-alias-test.vercel.app/</code>
+      </p>
+      <p>
+        It is supposed to render the proxy site at this path:{" "}
+        <code>`/vercelredirects`</code>
+      </p>
+      <p>Click the link below to demo</p>
+      <a href="https://gatsby-vercel-site-path-alias-test.vercel.app/vercelredirects">
+        https://gatsby-vercel-site-path-alias-test.vercel.app/vercelredirects
+      </a>
     </div>
   );
 }
